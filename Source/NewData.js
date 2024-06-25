ba1c1bf47bb845519d3750b59f1ea2c3b6b7b8f1
budgetalizera <117573813+vipulSP2108@users.noreply.github.com>
2024-06-25 13:54:50 +0530

    DataAdded

---
+++ Source/NewData.js
@@ -0,0 +1,145 @@
+const menuData = [
+    {
+      uniqueId: "o1",
+      outlet: "Tea Post",
+      menu: [
+        {
+          uniqueId: "c1",
+          title: "Beverages",
+          items: [
+            { uniqueId: "i1", item: "Cold Coffee" },
+            { uniqueId: "i2", item: "Iced Tea" },
+            { uniqueId: "i3", item: "Masala Chai" },
+            { uniqueId: "i4", item: "Cold Coco" }
+          ]
+        },
+        {
+          uniqueId: "c2",
+          title: "Snacks",
+          items: [
+            { uniqueId: "i5", item: "Sandwich" },
+            { uniqueId: "i6", item: "Maggi" },
+            { uniqueId: "i7", item: "Noodles" },
+            { uniqueId: "i9", item: "Puff" }
+          ]
+        }
+      ]
+    },
+    {
+      uniqueId: "o2",
+      outlet: "Amul Store",
+      menu: [
+        {
+          uniqueId: "c7",
+          title: "Desserts",
+          items: [
+            { uniqueId: "i18", item: "Amul TriCone" },
+            { uniqueId: "i19", item: "Amul Chocominis" }
+          ]
+        },
+        {
+          uniqueId: "c1",
+          title: "Beverages",
+          items: [
+            { uniqueId: "i17", item: "Amul Kool" }
+          ]
+        }
+      ]
+    },
+    {
+      uniqueId: "o3",
+      outlet: "Dawat Foods",
+      menu: [
+        {
+          uniqueId: "c3",
+          title: "Meals",
+          items: [
+            { uniqueId: "i13", item: "Chicken Rice" },
+            { uniqueId: "i14", item: "Chicken Curry" },
+            { uniqueId: "i15", item: "Thali" },
+            { uniqueId: "i16", item: "Masala Dosa" }
+          ]
+        },
+        {
+          uniqueId: "c11",
+          title: "Roll",
+          items: [
+            { uniqueId: "i8", item: "Egg Roll" }
+          ]
+        }
+      ]
+    },
+    {
+      uniqueId: "o4",
+      outlet: "JK Grocery and General Store",
+      menu: [
+        {
+          uniqueId: "c5",
+          title: "Fruits",
+          items: [
+            { uniqueId: "i22", item: "Banana" },
+            { uniqueId: "i23", item: "Apple" },
+            { uniqueId: "i24", item: "Kiwi" }
+          ]
+        },
+        {
+          uniqueId: "c1",
+          title: "Beverages",
+          items: [
+            { uniqueId: "i20", item: "Mango Shake" },
+            { uniqueId: "i21", item: "Orange Juice" }
+          ]
+        }
+      ]
+    },
+    {
+      uniqueId: "o5",
+      outlet: "Pooja Stationery",
+      menu: [
+        {
+          uniqueId: "c4",
+          title: "Stationery",
+          items: [
+            { uniqueId: "i25", item: "Classmate Pulse Subject Notebook" },
+            { uniqueId: "i26", item: "Doms Pencil Sets" },
+            { uniqueId: "i27", item: "Parker Pen" }
+          ]
+        }
+      ]
+    },
+    {
+      uniqueId: "o6",
+      outlet: "2 Degree Café",
+      menu: [
+        {
+          uniqueId: "c1",
+          title: "Beverages",
+          items: [
+            { uniqueId: "i1", item: "Cold Coffee" },
+            { uniqueId: "i2", item: "Iced Tea" },
+            { uniqueId: "i3", item: "Masala Chai" },
+            { uniqueId: "i4", item: "Cold Coco" }
+          ]
+        },
+        {
+          uniqueId: "c3",
+          title: "Meals",
+          items: [
+            { uniqueId: "i10", item: "Poha" },
+            { uniqueId: "i11", item: "Thepla" },
+            { uniqueId: "i12", item: "Upma" }
+          ]
+        },
+        {
+          uniqueId: "c2",
+          title: "Snacks",
+          items: [
+            { uniqueId: "i5", item: "Sandwich" },
+            { uniqueId: "i6", item: "Maggi" },
+            { uniqueId: "i7", item: "Noodles" },
+            { uniqueId: "i9", item: "Puff" }
+          ]
+        }
+      ]
+    }
+  ];
