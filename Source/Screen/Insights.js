e512da4434074c97b0f7e8b100eeef608b41c052
budgetalizera <117573813+vipulSP2108@users.noreply.github.com>
2024-07-08 21:14:35 +0530

    Major Fuctions of Statas screen

--- Source/Screen/Insights.js
+++ Source/Screen/Insights.js
@@ -3,6 +3,7 @@ import React, { useContext } from 'react'
 import { BarChart, PieChart } from 'react-native-gifted-charts';
 import { LinearGradient } from 'expo-linear-gradient';
 import { GlobalStateContext } from '../Context/GlobalStateContext';
+import Colors from '../Components/Colors';
 
 const pieData1 = [
   { value: 70, color: '#177AD5' },
@@ -38,46 +39,11 @@ const data = [
   { value: 2800, frontColor: '#3BE9DE', gradientColor: '#93FCF8' },
 ];
 
-const stackData = [
-  {
-    stacks: [
-      { value: 10, color: 'orange' },
-      { value: 20, color: '#4ABFF4', marginBottom: 2 },
-    ],
-    label: 'Jan',
-  },
-  {
-    stacks: [
-      { value: 10, color: '#4ABFF4' },
-      { value: 11, color: 'orange', marginBottom: 2 },
-      { value: 15, color: '#28B2B3', marginBottom: 2 },
-    ],
-    label: 'Mar',
-  },
-  {
-    stacks: [
-      { value: 14, color: 'orange' },
-      { value: 18, color: '#4ABFF4', marginBottom: 2 },
-    ],
-    label: 'Feb',
-  },
-  {
-    stacks: [
-      { value: 7, color: '#79C3DB' },
-      { value: 11, color: '#28B2B3', marginBottom: 2 },
-      { value: 10, color: '#4ABFF4', marginBottom: 2 },
-    ],
-    label: 'Mar',
-  },
-];
-
 export default function Insights() {
   const { dateGroup } = useContext(GlobalStateContext);
 
-  console.log(dateGroup)
-  
   function getLast7DaysLabels() {
-    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
     const today = new Date();
     const last7Days = [];
     for (let i = 6; i >= 0; i--) {
@@ -87,23 +53,22 @@ export default function Insights() {
     }
     return last7Days;
   }
-  
+
   function formatDataForBarPlot(data) {
     const today = new Date();
     const last7DaysTotals = new Array(7).fill(0);
-  
+
     // Iterate over each entry in the data
     data.forEach(entry => {
       const entryDate = new Date(entry.Noformatdate); //entry.date => Monday, July 8th 2024
-      console.log(entry)
       const diffDays = Math.floor((today - entryDate) / (1000 * 60 * 60 * 24));
-      
+
       if (diffDays >= 0 && diffDays < 7) {
         const dayIndex = 6 - diffDays;
         last7DaysTotals[dayIndex] += entry.total;
       }
     });
-  
+
     // Prepare the bar data
     const last7DaysLabels = getLast7DaysLabels();
     const barData = last7DaysTotals.map((total, index) => ({
@@ -111,12 +76,158 @@ export default function Insights() {
       label: last7DaysLabels[index],
       frontColor: '#4ABFF4' // Default color for bar data
     }));
-  
+
     return barData;
   }
-  
+
   const barData = formatDataForBarPlot(dateGroup);
   console.log(dateGroup);
+  const categoryData = [
+    { 'type': 'Veg', 'color': '#00e676' },             
+    { 'type': 'NonVeg', 'color': '#ff0000' },          
+    { 'type': 'Stationery', 'color': "#0092ff" },     
+    { 'type': 'Beverage', 'color': '#4ABFF4' },        
+    { 'type': 'Hot_Cafe', 'color': '#923c01' },       
+    { 'type': 'Cold_Cafe', 'color': '#c37960' },       
+    { 'type': 'Snacks', 'color': 'orange' },        
+    { 'type': 'Hot_Meal', 'color': '#ffb80e' },   
+    { 'type': 'Cold_Dessert', 'color': '#FF4191' },  
+    { 'type': 'Cold_Beverage', 'color': '#4ABFF4' },  
+    { 'type': 'Fresh', 'color': 'green' },         
+    { 'type': 'Hot_Snacks', 'color': '#ff611d' },    
+    { 'type': 'Bakery_Dessert', 'color': '#FF4191' },  
+    { 'type': 'Bakery_Bread', 'color': '#efa14b' },   
+  ];
+
+  function formatDataForStackedBarPlot(data) {
+    const today = new Date();
+    const last7DaysTotals = new Array(7).fill(0);
+    const last7DaysItems = new Array(7).fill(null).map(() => ({})); // Initialize each element with a separate object
+  
+    // Iterate over each entry in the data
+    data.forEach(entry => {
+      const entryDate = new Date(entry.Noformatdate);
+      const diffDays = Math.floor((today - entryDate) / (1000 * 60 * 60 * 24));
+  
+      if (diffDays >= 0 && diffDays < 7) {
+        const dayIndex = 6 - diffDays;
+  
+        entry.orders.forEach(order => {
+          order.items.forEach(item => {
+            const itemType = item.category;
+            const itemPrice = parseFloat(item.price) * parseFloat(item.quantity); // Assuming price is a string, convert to float
+            
+            // Check if itemType exists in categoryData
+            let categoryInfo = categoryData.find(category => category.type === itemType);
+            
+            // If itemType doesn't exist, assign it to 'Others' with gray color
+            if (!categoryInfo) {
+              categoryInfo = { type: 'Others', color: '#cccccc' }; // Gray color for others
+            }
+  
+            // Initialize the item type if it doesn't exist for this day
+            if (!last7DaysItems[dayIndex][categoryInfo.type]) {
+              last7DaysItems[dayIndex][categoryInfo.type] = { total: itemPrice, color: categoryInfo.color };
+            } else {
+              last7DaysItems[dayIndex][categoryInfo.type].total += itemPrice; // Add to existing type's total
+            }
+          });
+        });
+  
+        // Accumulate total for the day
+        last7DaysTotals[dayIndex] += entry.total;
+      }
+    });
+  
+    // Prepare the stacked bar data
+    const last7DaysLabels = getLast7DaysLabels();
+    const stackData = last7DaysTotals.map((total, index) => {
+      const stacks = Object.entries(last7DaysItems[index]).map(([type, { total, color }]) => ({
+        value: total,
+        color,
+      }));
+  
+      // If stacks array is empty, add default stack
+      if (stacks.length === 0) {
+        stacks.push({
+          value: 0,
+          color: 'grey',
+        });
+      }
+  
+      return {
+        stacks,
+        label: last7DaysLabels[index]
+      };
+    });
+  
+    return stackData;
+  }
+  
+  const barData2 = [
+    {
+      value: 40,
+      label: 'Jan',
+      spacing: 2,
+      labelWidth: 30,
+      labelTextStyle: {color: 'gray'},
+      frontColor: '#177AD5',
+    },
+    {value: 20, frontColor: '#ED6665'},
+    {
+      value: 50,
+      label: 'Feb',
+      spacing: 2,
+      labelWidth: 30,
+      labelTextStyle: {color: 'gray'},
+      frontColor: '#177AD5',
+    },
+    {value: 40, frontColor: '#ED6665'},
+    {
+      value: 75,
+      label: 'Mar',
+      spacing: 2,
+      labelWidth: 30,
+      labelTextStyle: {color: 'gray'},
+      frontColor: '#177AD5',
+    },
+    {value: 25, frontColor: '#ED6665'},
+    {
+      value: 30,
+      label: 'Apr',
+      spacing: 2,
+      labelWidth: 30,
+      labelTextStyle: {color: 'gray'},
+      frontColor: '#177AD5',
+    },
+    {value: 20, frontColor: '#ED6665'},
+    {
+      value: 60,
+      label: 'May',
+      spacing: 2,
+      labelWidth: 30,
+      labelTextStyle: {color: 'gray'},
+      frontColor: '#177AD5',
+    },
+    {value: 40, frontColor: '#ED6665'},
+    {
+      value: 65,
+      label: 'Jun',
+      spacing: 2,
+      labelWidth: 30,
+      labelTextStyle: {color: 'gray'},
+      frontColor: '#177AD5',
+    },
+    {value: 30, frontColor: '#ED6665'},
+  ];
+
+  
+
+  const chartData = formatDataForStackedBarPlot(dateGroup);
+  console.log('chartData', chartData);
+  chartData.forEach((item) => {
+      console.log(item)
+  })
 
   // const renderDot = color => {
   //   return (
@@ -180,101 +291,109 @@ export default function Insights() {
 
   return (
     <ScrollView>
+      <View style={{ padding: 20, alignItems: 'center' }}>
+        <BarChart
+          isAnimated
+          barWidth={18}
+          noOfSections={5}
+          barBorderRadius={4}
+          frontColor="lightgray"
+          data={barData}
+          yAxisThickness={0}
+          xAxisThickness={0}
+
+          showReferenceLine1
+          referenceLine1Position={barData.reduce((sum, item) => sum + item.value / 2, 0) / barData.length}
+          referenceLine1Config={{
+            color: 'gray',
+            dashWidth: 2,
+            dashGap: 3,
+          }}
+
+          showLine
+          lineConfig={{
+            color: '#F29C6E',
+            thickness: 3
+          }}
+        />
+      </View>
+      {console.log('barData', barData)}
       <View
         style={{
           paddingVertical: 100,
-          backgroundColor: '#34448B',
+          backgroundColor: Colors.dark.colors.backGroundColor,
           flex: 1,
         }}>
-
+<BarChart
+textShiftX={10}
+          data={barData2}
+          barWidth={8}
+          spacing={24}
+          roundedTop
+          roundedBottom
+          hideRules
+          xAxisThickness={0}
+          yAxisThickness={0}
+          yAxisTextStyle={{color: 'gray'}}
+          noOfSections={3}
+          maxValue={75}
+        />
         <View
+        className='px-4'
           style={{
-            margin: 10,
-            padding: 16,
-            borderRadius: 20,
-            backgroundColor: '#232B5D',
+            // marginHorizontal: 40,
+          // justifyContent: 'center',
+          // alignItems: 'center',
+            // padding: 16,
+            // borderRadius: 20,
+            backgroundColor: Colors.dark.colors.componentColor,
           }}>
-          <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>
+          {/* <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>
             Overview
-          </Text>
+          </Text> */}
+
           <BarChart
-          barBorderRadius={1}
-          // spacing={40}
-            stackData={stackData}
-            barWidth={10}
-            noOfSections={3}
             
-            frontColor="lightgray"
-            data={barData}
+            disableScroll
+          adjustToWidth
+            isAnimated
+            barWidth={8}
+            spacing={25}
+            noOfSections={5}
+            initialSpacing={5}
+            rulesColor={Colors.dark.colors.backGroundColor}
             yAxisThickness={0}
             xAxisThickness={0}
-
+            // hideRules
+            autoShiftLabels
+            stackData={chartData}
+            yAxisTextStyle={{color: 'gray', fontSize: 14}}
+            xAxisLabelTextStyle={{color: 'gray', fontSize: 14, marginLeft: -5}}//{{color: 'gray'}}
+            stackBorderTopRightRadius={14}
+            stackBorderTopLeftRadius={14}
+            showReferenceLine1
             referenceLine1Position={20} //Avrage
-              referenceLine1Config={{
-                color: 'gray',
-                dashWidth: 2,
-                dashGap: 3,
-              }}
-              
-              showLine
-              lineConfig={{
-                color: '#F29C6E',
-                thickness: 3,
-                curved: true,
-                hideDataPoints: true,
-                shiftY: 6,
-                // initialSpacing: -30,
-              }}
+            referenceLine1Config={{
+              color: 'gray',
+              dashWidth: 2,
+              dashGap: 3,
+            }}
 
-              hideRules
-
-              isAnimated
-              showReferenceLine1
+            // showLine
+            // lineConfig={{
+            //   color: Colors.dark.colors.diffrentColorOrange,
+            //   thickness: 3
+            // }}
           />
-          <View style={{ padding: 20, alignItems: 'center' }}>
-            <BarChart
-              barWidth={22}
-              noOfSections={3}
-              barBorderRadius={4}
-              frontColor="lightgray"
-              data={barData}
-              yAxisThickness={0}
-              xAxisThickness={0}
-              
-              
-              
-              hideRules
 
-              isAnimated
-              showReferenceLine1
-              
-              
-              referenceLine1Position={120} //Avrage
-              referenceLine1Config={{
-                color: 'gray',
-                dashWidth: 2,
-                dashGap: 3,
-              }}
-              
-              showLine
-              lineConfig={{
-                color: '#F29C6E',
-                thickness: 3,
-                curved: true,
-                hideDataPoints: true,
-                shiftY: 6,
-                // initialSpacing: -30,
-              }}
-            />
-          </View>
         </View>
 
         <View
           style={{
-            margin: 20,
-            padding: 16,
+            margin: 100,
+            // padding: 106,
             borderRadius: 20,
-            backgroundColor: '#232B5D',
+            backgroundColor:Colors.dark.colors.componentColor,
           }}>
           <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>
             Performance
--- Source/Screen/Like.js
+++ Source/Screen/Like.js
@@ -1,75 +1,58 @@
-import { View, Text, TouchableOpacity, ImageBackground } from 'react-native'
-import React from 'react'
-import Colors from '../Components/Colors';
-import LongStarIcon from '../Components/LongStarIcon';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Colors from "../Components/Colors";
 
-export default function Like() {
-  const text = "Title Name Name"
-  const index = text.indexOf(' ');
+const categoryData = [
+  { 'type': 'Veg', 'color': '#00e676' },             
+  { 'type': 'NonVeg', 'color': '#ff0000' },          
+  { 'type': 'Stationery', 'color': 'blue' },     
+  { 'type': 'Beverage', 'color': '#4ABFF4' },        
+  { 'type': 'Hot_Cafe', 'color': '#923c01' },       
+  { 'type': 'Cold_Cafe', 'color': '#c37960' },       
+  { 'type': 'Snacks', 'color': '#ff611d' },        
+  { 'type': 'Hot_Meal', 'color': '#ffb80e' },   
+  { 'type': 'Cold_Dessert', 'color': '#FF4191' },  
+  { 'type': 'Cold_Beverage', 'color': '#4ABFF4' },  
+  { 'type': 'Fresh', 'color': 'green' },         
+  { 'type': 'Hot_Snacks', 'color': '#ff611d' },    
+  { 'type': 'Bakery_Dessert', 'color': '#FF4191' },  
+  { 'type': 'Bakery_Bread', 'color': '#efa14b' },   
+];
 
-  var output;
-  if (index !== -1) {
-    const part1 = text.substring(0, index);
-    const part2 = text.substring(index + 1);
-    output = `${part1}\n${part2}`;
-
-  } else {
-    // If there's no space, return the original text
-    output = text
-  }
-
-
+const Like = () => {
   return (
-    <View className=' w-full h-full' style={{ backgroundColor: Colors.dark.colors.backGroundColor }}>
-      <View className=' h-3/5' style={{ borderBottomRightRadius: 100, backgroundColor: Colors.dark.colors.secComponentColor }} >
-        <ImageBackground
-          source={{
-            uri: "https://www.teacupsfull.com/cdn/shop/articles/Screenshot_2023-10-20_at_11.07.13_AM.png?v=1697780292",
-            method: 'POST',
-            headers: {
-              Pragma: 'no-cache',
-            },
-          }}
-          defaultSource={require('./../../assets/favicon.png')}
-          alt="Logo"
-          className='h-full overflow-hidden'
-          style={{ borderBottomRightRadius: 100 }}
-        >
-        <LinearGradient
-          start={{ x: 0.3, y: 0.4 }} end={{ x: 0.1, y: 1 }}
-          className='overflow-hidden h-full w-full'
-          colors={['transparent', Colors.dark.colors.backGroundColor]}
-        ></LinearGradient>
-        </ImageBackground>
-        <View className='w-full absolute bottom-0 p-4'>
-          <View className=' flex-row w-full'>
-            <View>
-              <Text className=' font-black text-4xl' style={{ color: Colors.dark.colors.mainTextColor }}>
-                {output}
-              </Text>
-              <View className='py-4'>
-                <LongStarIcon rating={3.5} ratingcount={200} gaps={20} border={0} size={20} backGround={'transparent'}/>
-              </View>
-            </View>
-          </View>
-          <View>
-            <Text className=' absolute font-black text-xl py-2 px-4 rounded-xl' style={{ color: Colors.dark.colors.mainTextColor, backgroundColor: Colors.dark.colors.diffrentColorOrange }}>
-              Rs 200
-            </Text>
-          </View>
+    <View className='h-full' style={styles.container}>
+      {categoryData.map((category, index) => (
+        <View key={index} style={[styles.categoryBox, { backgroundColor: category.color }]}>
+          <Text style={styles.categoryText}>{category.type}</Text>
         </View>
-      </View>
-
-
-      <View className='mt-5 p-4'>
-        <Text className=' font-black text-3xl py-4' style={{ color: Colors.dark.colors.mainTextColor, }}>
-          {text}
-        </Text>
-        <Text className=' font-normal text-lg' style={{ color: Colors.dark.colors.textColor, }}>
-          {text}{text}{text}{text}{text}{text}
-        </Text>
-      </View>
+      ))}
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.dark.colors.backGroundColor,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  categoryBox: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5,
+    borderRadius: 10,
+  },
+  categoryText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#ffffff', // White text color
+  },
+});
+
+export default Like;
